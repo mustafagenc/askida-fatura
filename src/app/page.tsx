75c42ce58c9b1d8f4dc421a3e45c234a058a842d
Mustafa Genc <eposta@mustafagenc.info>
2025-10-07 20:17:42 +0300

    Feat: Adds favicon and logo assets
    Adds new favicon images for various resolutions and platforms.
    Includes a new logo image for improved branding.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-    return <>Test Deneme 1 2 3</>;
+    return (
+        <div className="w-full flex flex-col justify-center items-center h-screen gap-10">
+            <Image
+                src={'/images/logo/askidafatura.png'}
+                width={128}
+                height={128}
+                alt="Askıda Fatura"
+            />
+            <h1 className="text-3xl font-bold text-center">Askıda Fatura</h1>
+        </div>
+    );
 }
